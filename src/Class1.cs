ea63cd03edef6f5cf4b76f5e22277398859a1e09
Jenny Bai <v-jennybai@microsoft.com>
2023-05-10 15:02:50 +0800

    Add project

---
+++ src/Class1.cs
@@ -0,0 +1,10 @@
+﻿namespace Issue8735
+{
+    public class Class1
+    {
+        public string JoinNames(string firstName, string lastName)
+        {
+            return firstName + " " + lastName;
+        }
+    }
+}
